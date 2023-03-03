40b0875818adc0ba39d96d1956b6a683d43fd0a9
henrique11varela <henrique11varela@gmail.com>
2023-03-03 11:57:22 +0000

    storing

--- app.js
+++ app.js
@@ -1,221 +1,224 @@
-window.addEventListener('load', () => {
+/* window.addEventListener('load', () => { */
+const canvas = document.getElementById("canvas");
+const vmin = window.innerWidth < window.innerHeight ? window.innerWidth : window.innerHeight;
+canvas.height = vmin * .8;
+canvas.width = vmin * .8;
+const rect = canvas.getBoundingClientRect();
+const ctx = canvas.getContext('2d');
+const pixelGap = 2 //space between cells
 
-    //*Constants
-    const vmin = window.innerWidth < window.innerHeight ? window.innerWidth : window.innerHeight;
-    const canvas = document.getElementById("canvas");
-    canvas.height = vmin * .8;
-    canvas.width = vmin * .8;
-    const rect = canvas.getBoundingClientRect();
-    const ctx = canvas.getContext('2d');
-    const framerateInput = document.getElementById("framerate");
-    const resolutionInput = document.getElementById("resolution");
-    const playBtn = document.getElementById("btn-play");
-    const stepBtn = document.getElementById("btn-step");
-    const pixelGap = 2; //space between cells
-
-    //*Settings
-    let framerate = 10; //rough framerate of animation
-    let resolution; //number of cells on smallest side
-    if (window.innerWidth > 768) {
-        resolution = 40;
-    } else {
-        resolution = 20;
+let settings = {
+    elements: {
+        framerate: document.getElementById("framerate"),
+        resolution: document.getElementById("resolution"),
+        playBtn: document.getElementById("btn-play"),
+        stepBtn: document.getElementById("btn-step")
+    },
+    values: {
+        framerate: 10, //rough framerate of animation
+        resolution: 20 //number of cells on smallest side
     }
-    let pixelSize = canvas.width / resolution; //size of cell
+};
 
-    //*GLOBALS
-    //Innerworkings
-    let mat = [];
-    //Draw
-    let coordinateX = -1;
-    let coordinateY = -1;
-    let color;
-    //Animation interval
-    let intervalID;
+//*Constants
 
-    //! SETUP
-    framerateInput.value = framerate;
-    resolutionInput.value = resolution;
+//*Settings
+if (window.innerWidth > 768) {
+    settings.values.resolution = 40;
+}
+let pixelSize = canvas.width / settings.values.resolution; //size of cell
 
-    //! EVENTS
-    // Button events
-    playBtn.addEventListener('click', startAnimation);
-    stepBtn.addEventListener('click', animate);
+//*GLOBALS
+//Innerworkings
+let mat = [];
+//Draw
+let coordinateX = -1;
+let coordinateY = -1;
+let color;
+//Animation interval
+let intervalID;
 
-    // resolution Enter
-    resolutionInput.addEventListener('keydown', (event) => {
-        if (event.key === "Enter" && resolutionInput.value != resolution) {
-            resolution = parseInt(resolutionInput.value);
-            buildMat();
-            pixelSize = canvas.width / resolution;
-            draw();
-        }
-    });
-    
-    // framerate Enter
-    framerateInput.addEventListener('keydown', (event) => {
-        if (event.key === "Enter" && framerateInput.value != framerate) {
-            framerate = parseInt(framerateInput.value);
+//! SETUP
+settings.elements.framerate.value = settings.values.framerate;
+settings.elements.resolution.value = settings.values.resolution;
+
+function changeValueFramerate(value) {
+    if (settings.values.framerate + value > 0) {
+        settings.values.framerate += value;
+        settings.elements.framerate.innerHTML = settings.values.framerate;
+        if (settings.elements.playBtn.innerHTML == "PAUSE") {
             stopAnimation();
             startAnimation();
         }
-    });
+    }
+}
 
-    // canvas Draw mouse
-    canvas.addEventListener('mousedown', onMouseDown = (event) => { //MOUSE DOWN
-        let coordinateX = Math.floor((event.clientX - rect.left) / pixelSize);
-        let coordinateY = Math.floor((event.clientY - rect.top) / pixelSize);
-        color = !mat[coordinateX][coordinateY];
-        mat[coordinateX][coordinateY] = color;
-        ctx.fillStyle = color ? "black" : "white";
-        ctx.fillRect(coordinateX * pixelSize, coordinateY * pixelSize, pixelSize - pixelGap, pixelSize - pixelGap);
-        canvas.addEventListener('mousemove', onMouseMove = (event) => { //MOUSE MOVE
-            let tempX = Math.floor((event.clientX - rect.left) / pixelSize);
-            let tempY = Math.floor((event.clientY - rect.top) / pixelSize);
-            if (coordinateX != tempX || coordinateY != tempY) {
-                coordinateX = tempX;
-                coordinateY = tempY;
-                mat[coordinateX][coordinateY] = color;
-                ctx.fillStyle = color ? "black" : "white";
-                ctx.fillRect(coordinateX * pixelSize, coordinateY * pixelSize, pixelSize - pixelGap, pixelSize - pixelGap);
-            }
-        });
-    });
+function changeValueResolution(value) {
+    if (settings.values.resolution + value > 0) {
+        settings.values.resolution += value;
+        settings.elements.resolution.innerHTML = settings.values.resolution;
+        buildMat();
+        pixelSize = canvas.width / settings.values.resolution;
+        draw();
+    }
+}
 
-    canvas.addEventListener('mouseup', onMouseUp = () => { //MOUSE UP
-        coordinateX = -1;
-        coordinateY = -1;
-        canvas.removeEventListener('mousemove', onMouseMove);
-    });
+//! EVENTS
+// Button events
+settings.elements.playBtn.addEventListener('click', startAnimation);
+settings.elements.stepBtn.addEventListener('click', animate);
 
-    // canvas Draw touch
-    canvas.addEventListener('touchstart', onTouchDown = (event) => { //Touch DOWN
-        let coordinateX = Math.floor((event.changedTouches[0].clientX - rect.left) / pixelSize);
-        let coordinateY = Math.floor((event.changedTouches[0].clientY - rect.top) / pixelSize);
-        color = !mat[coordinateX][coordinateY];
-        mat[coordinateX][coordinateY] = color;
-        ctx.fillStyle = color ? "white" : "black";
-        ctx.fillRect(coordinateX * pixelSize, coordinateY * pixelSize, pixelSize - pixelGap, pixelSize - pixelGap);
-        canvas.addEventListener('touchmove', onTouchMove = (event) => { //Touch MOVE
-            let tempX = Math.floor((event.changedTouches[0].clientX - rect.left) / pixelSize);
-            let tempY = Math.floor((event.changedTouches[0].clientY - rect.top) / pixelSize);
-            if (coordinateX != tempX || coordinateY != tempY) {
-                coordinateX = tempX;
-                coordinateY = tempY;
-                mat[coordinateX][coordinateY] = color;
-                ctx.fillStyle = color ? "white" : "black";
-                ctx.fillRect(coordinateX * pixelSize, coordinateY * pixelSize, pixelSize - pixelGap, pixelSize - pixelGap);
-            }
-        });
-    });
-
-    canvas.addEventListener('touchend', onTouchUp = () => { //Touch UP
-        coordinateX = -1;
-        coordinateY = -1;
-        canvas.removeEventListener('touchmove', onTouchMove);
-    });
-    
-    //! ANIMATION
-    function startAnimation() {
-        framerate = framerateInput.value;
-        if (framerate < 1) {
-            framerate = 1;
-            framerateInput.value = 1;
-        } else if (framerate > 60) {
-            framerate = 60;
-            framerateInput.value = 60;
+// canvas Draw mouse
+canvas.addEventListener('mousedown', onMouseDown = (event) => { //MOUSE DOWN
+    let coordinateX = Math.floor((event.clientX - rect.left) / pixelSize);
+    let coordinateY = Math.floor((event.clientY - rect.top) / pixelSize);
+    color = !mat[coordinateX][coordinateY];
+    mat[coordinateX][coordinateY] = color;
+    ctx.fillStyle = color ? "black" : "white";
+    ctx.fillRect(coordinateX * pixelSize, coordinateY * pixelSize, pixelSize - pixelGap, pixelSize - pixelGap);
+    canvas.addEventListener('mousemove', onMouseMove = (event) => { //MOUSE MOVE
+        let tempX = Math.floor((event.clientX - rect.left) / pixelSize);
+        let tempY = Math.floor((event.clientY - rect.top) / pixelSize);
+        if (coordinateX != tempX || coordinateY != tempY) {
+            coordinateX = tempX;
+            coordinateY = tempY;
+            mat[coordinateX][coordinateY] = color;
+            ctx.fillStyle = color ? "black" : "white";
+            ctx.fillRect(coordinateX * pixelSize, coordinateY * pixelSize, pixelSize - pixelGap, pixelSize - pixelGap);
         }
-        intervalID = setInterval(() => { animate(); }, 1000 / framerate);
-        playBtn.removeEventListener('click', startAnimation);
-        playBtn.addEventListener('click', stopAnimation);
-        playBtn.innerHTML = "PAUSE";
-    }
-
-    function stopAnimation() {
-        clearInterval(intervalID);
-        intervalID = null;
-        playBtn.removeEventListener('click', stopAnimation);
-        playBtn.addEventListener('click', startAnimation);
-        playBtn.innerHTML = "PLAY";
-    }
-
-    function animate() {
-        update(mat);
-        draw(mat);
-    }
-
-    //! BUILD MAT
-    function buildMat() {
-        mat = [];
-        for (let i = 0; i < resolution; i++) {
-            mat.push([]);
-            for (let j = 0; j < resolution; j++) {
-                mat[i].push(false);
-            }
-        }
-    }
-
-    //! DRAW 
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        for (let i = 0; i < mat.length; i++) {
-            for (let j = 0; j < mat[i].length; j++) {
-                ctx.fillStyle = mat[i][j] ? "black" : "white";
-                ctx.fillRect(pixelSize * i, pixelSize * j, pixelSize - pixelGap, pixelSize - pixelGap);
-            }
-        }
-    }
-
-    //! UPDATE 
-    function update() {
-        let nm = [];
-        for (let i = 0; i < mat.length; i++) {
-            nm.push([]);
-            for (let j = 0; j < mat[i].length; j++) {
-                let counter = 0;
-                if (i != 0) {
-                    // look N
-                    counter += mat[i - 1][j] ? 1 : 0;
-                    if (j != 0) { // look NW
-                        counter += mat[i - 1][j - 1] ? 1 : 0;
-                    }
-                    if (j != mat[i].length - 1) { // look NE
-                        counter += mat[i - 1][j + 1] ? 1 : 0;
-                    }
-                }
-                if (i != mat.length - 1) {
-                    //look down
-                    counter += mat[i + 1][j] ? 1 : 0;
-                    if (j != 0) { // look SW
-                        counter += mat[i + 1][j - 1] ? 1 : 0;
-                    }
-                    if (j != mat[i].length - 1) { // look SE
-                        counter += mat[i + 1][j + 1] ? 1 : 0;
-                    }
-                }
-                if (j != 0) {
-                    //look W
-                    counter += mat[i][j - 1] ? 1 : 0;
-                }
-                if (j != mat[i].length - 1) {
-                    //look E
-                    counter += mat[i][j + 1] ? 1 : 0;
-                }
-
-                if (!mat[i][j] && counter == 3) {//if dead
-                    nm[i].push(true);
-                } else if (mat[i][j] && ((counter < 2) || (counter > 3))) { // if alive
-                    nm[i].push(false);
-                } else {
-                    nm[i].push(mat[i][j]);
-                }
-            }
-        }
-        mat = nm;
-    }
-
-    //! MAIN
-    buildMat();
-    update();
-    draw();
+    });
 });
+
+canvas.addEventListener('mouseup', onMouseUp = () => { //MOUSE UP
+    coordinateX = -1;
+    coordinateY = -1;
+    canvas.removeEventListener('mousemove', onMouseMove);
+});
+
+// canvas Draw touch
+canvas.addEventListener('touchstart', onTouchDown = (event) => { //Touch DOWN
+    let coordinateX = Math.floor((event.changedTouches[0].clientX - rect.left) / pixelSize);
+    let coordinateY = Math.floor((event.changedTouches[0].clientY - rect.top) / pixelSize);
+    color = !mat[coordinateX][coordinateY];
+    mat[coordinateX][coordinateY] = color;
+    ctx.fillStyle = color ? "white" : "black";
+    ctx.fillRect(coordinateX * pixelSize, coordinateY * pixelSize, pixelSize - pixelGap, pixelSize - pixelGap);
+    canvas.addEventListener('touchmove', onTouchMove = (event) => { //Touch MOVE
+        let tempX = Math.floor((event.changedTouches[0].clientX - rect.left) / pixelSize);
+        let tempY = Math.floor((event.changedTouches[0].clientY - rect.top) / pixelSize);
+        if (coordinateX != tempX || coordinateY != tempY) {
+            coordinateX = tempX;
+            coordinateY = tempY;
+            mat[coordinateX][coordinateY] = color;
+            ctx.fillStyle = color ? "white" : "black";
+            ctx.fillRect(coordinateX * pixelSize, coordinateY * pixelSize, pixelSize - pixelGap, pixelSize - pixelGap);
+        }
+    });
+});
+
+canvas.addEventListener('touchend', onTouchUp = () => { //Touch UP
+    coordinateX = -1;
+    coordinateY = -1;
+    canvas.removeEventListener('touchmove', onTouchMove);
+});
+
+//! ANIMATION
+function startAnimation() {
+    if (settings.values.framerate > 60) {
+        settings.values.framerate = 60;
+        settings.elements.framerate.value = 60;
+    }
+    intervalID = setInterval(() => { animate(); }, 1000 / settings.values.framerate);
+    settings.elements.playBtn.removeEventListener('click', startAnimation);
+    settings.elements.playBtn.addEventListener('click', stopAnimation);
+    settings.elements.playBtn.innerHTML = "PAUSE";
+}
+
+function stopAnimation() {
+    clearInterval(intervalID);
+    intervalID = null;
+    settings.elements.playBtn.removeEventListener('click', stopAnimation);
+    settings.elements.playBtn.addEventListener('click', startAnimation);
+    settings.elements.playBtn.innerHTML = "PLAY";
+}
+
+function animate() {
+    update(mat);
+    draw(mat);
+}
+
+//! BUILD MAT
+function buildMat() {
+    mat = [];
+    for (let i = 0; i < settings.values.resolution; i++) {
+        mat.push([]);
+        for (let j = 0; j < settings.values.resolution; j++) {
+            mat[i].push(false);
+        }
+    }
+}
+
+//! DRAW 
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < mat.length; i++) {
+        for (let j = 0; j < mat[i].length; j++) {
+            ctx.fillStyle = mat[i][j] ? "black" : "white";
+            ctx.fillRect(pixelSize * i, pixelSize * j, pixelSize - pixelGap, pixelSize - pixelGap);
+        }
+    }
+}
+
+//! UPDATE 
+function update() {
+    let nm = [];
+    for (let i = 0; i < mat.length; i++) {
+        nm.push([]);
+        for (let j = 0; j < mat[i].length; j++) {
+            let counter = 0;
+            if (i != 0) {
+                // look N
+                counter += mat[i - 1][j] ? 1 : 0;
+                if (j != 0) { // look NW
+                    counter += mat[i - 1][j - 1] ? 1 : 0;
+                }
+                if (j != mat[i].length - 1) { // look NE
+                    counter += mat[i - 1][j + 1] ? 1 : 0;
+                }
+            }
+            if (i != mat.length - 1) {
+                //look down
+                counter += mat[i + 1][j] ? 1 : 0;
+                if (j != 0) { // look SW
+                    counter += mat[i + 1][j - 1] ? 1 : 0;
+                }
+                if (j != mat[i].length - 1) { // look SE
+                    counter += mat[i + 1][j + 1] ? 1 : 0;
+                }
+            }
+            if (j != 0) {
+                //look W
+                counter += mat[i][j - 1] ? 1 : 0;
+            }
+            if (j != mat[i].length - 1) {
+                //look E
+                counter += mat[i][j + 1] ? 1 : 0;
+            }
+
+            if (!mat[i][j] && counter == 3) {//if dead
+                nm[i].push(true);
+            } else if (mat[i][j] && ((counter < 2) || (counter > 3))) { // if alive
+                nm[i].push(false);
+            } else {
+                nm[i].push(mat[i][j]);
+            }
+        }
+    }
+    mat = nm;
+}
+
+//! MAIN
+buildMat();
+update();
+draw();
+/* }); */
